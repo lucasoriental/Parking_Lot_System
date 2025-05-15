922fa797056510b377f686b3a72e832e2eaa0649
lucasoriental <lucasoriental16@gmail.com>
2025-05-15 16:36:11 +0100

    Just organizing some files.

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,10 +1,10 @@
 import { Pool } from "pg";
 import "./env.config.js";
 
-const username = process.env.PG_USERNAME
-const password = process.env.PG_PASSWORD
-const host = process.env.PG_HOST
-const dbname = process.env.PG_DBNAME
+const username = process.env.PG_USERNAME;
+const password = process.env.PG_PASSWORD;
+const host = process.env.PG_HOST;
+const dbname = process.env.PG_DBNAME;
 
 const database_URL = `postgres://${username}:${password}@${host}/${dbname}`;
 
--- server/test/testConnection.js
+++ server/test/testConnection.js
@@ -1,8 +1,17 @@
 import pool from "../config/db.config.js";
-import '../config/env.config.js'
+import "../config/env.config.js";
 
 async function testConnection() {
-    console.log("UserName:", process.env.PG_USERNAME,"\nPassword:", process.env.PG_PASSWORD,"\nHost:", process.env.PG_HOST,"\nDB Name:", process.env.PG_DBNAME)
+  console.log(
+    "UserName:",
+    process.env.PG_USERNAME,
+    "\nPassword:",
+    process.env.PG_PASSWORD,
+    "\nHost:",
+    process.env.PG_HOST,
+    "\nDB Name:",
+    process.env.PG_DBNAME
+  );
   try {
     const conexao = await pool.query("SELECT NOW()");
     console.log(conexao.rows[0]);
