233078a52389dc2561d5d39ce720a6c8a6bba33c
lucasoriental <lucasoriental16@gmail.com>
2025-05-15 16:19:12 +0100

    New endpoints for the system; One of them is temporary, only for taking care of tables creation, and the other one is for manipulate the tables already created. For now, only retrieve all the data is ready.

--- server/index.js
+++ server/index.js
@@ -2,13 +2,16 @@ import express from "express";
 import cors from "cors";
 import "./config/env.config.js";
 
+import tables from "./db/tables/common.routes.js";
+import carsRoutes from "./routes/car.routes.js";
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/parking", (r,q) => {q.send("Olá"); console.log("ola")});
+app.use("/api/cars", carsRoutes);
+app.use("/scripts/table", tables);
 
-app.listen(PORT); //Required
-
+app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`));
